9e8872fdf57179e51e2dfba24b2a33e818320462
WasakaBe <wasakabeofficial@gmail.com>
2026-03-07 18:43:42 -0600

    Creation of the first UI component "DataGuard" along with its stylesheet and testing

---
+++ src/components/ui/DataGuard.tsx
@@ -0,0 +1,25 @@
+import { useEffect } from "react";
+import type { DataGuardProps } from "../../types/Data.Guard.type";
+
+const DataGuard = ({
+  isDirty,
+  customMessage = "Tienes cambios sin guardar. Si sales, perderás toda la información del CV.",
+}: DataGuardProps) => {
+  useEffect(() => {
+    const handleBeforeUnload = (event: BeforeUnloadEvent) => {
+      if (isDirty) {
+        event.preventDefault();
+        event.returnValue = customMessage;
+
+        return customMessage;
+      }
+    };
+    window.addEventListener("beforeunload", handleBeforeUnload);
+    return () => {
+      window.removeEventListener("beforeunload", handleBeforeUnload);
+    };
+  }, [isDirty, customMessage]);
+  return null;
+};
+
+export default DataGuard;
--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -6,4 +6,15 @@ import DateField from "./DateField";
 import Button from "./Button";
 import Table from "./Table";
 import SectionCard from "./SectionCard";
-export { Title, Label, Input, TextArea, DateField, Button, Table, SectionCard };
+import DataGuard from "./DataGuard";
+export {
+  Title,
+  Label,
+  Input,
+  TextArea,
+  DateField,
+  Button,
+  Table,
+  SectionCard,
+  DataGuard,
+};
