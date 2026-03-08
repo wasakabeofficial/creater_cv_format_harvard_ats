bb176f0e5a23099e792636092cd8de27e0c204c3
WasakaBe <wasakabeofficial@gmail.com>
2026-03-08 09:55:42 -0600

    Update Button type

--- src/types/ui/Button.type.ts
+++ src/types/ui/Button.type.ts
@@ -2,7 +2,7 @@ export interface ButtonProps {
   label: string;
   onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void;
   type?: "button" | "submit" | "reset";
-  variant?: "primary" | "secondary" | "danger";
+  variant?: "primary" | "secondary" | "danger" | "outline";
   disabled?: boolean;
   className?: string;
   fullWidth?: boolean;
