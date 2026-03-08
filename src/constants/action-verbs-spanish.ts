52b8c195e9fdb58151e312c8e8577d4a57546a8d
WasakaBe <wasakabeofficial@gmail.com>
2026-03-07 19:17:18 -0600

    add bilingual Harvard action verbs and weak verbs constants

---
+++ src/constants/action-verbs-spanish.ts
@@ -0,0 +1,62 @@
+export const LEADERSHIP_VERBS_SPANISH = [
+  "Aceleré",
+  "Dirigí",
+  "Ejecuté",
+  "Fundé",
+  "Guié",
+  "Gestioné",
+  "Orquesté",
+  "Lideré",
+  "Supervisé",
+  "Transformé",
+  "Presidí",
+  "Coordiné",
+] as const;
+
+export const TECHNICAL_VERBS_SPANISH = [
+  "Analicé",
+  "Construí",
+  "Programé",
+  "Computé",
+  "Depuré",
+  "Diseñé",
+  "Desarrollé",
+  "Implementé",
+  "Optimicé",
+  "Reestructuré",
+  "Automaticé",
+  "Instalé",
+] as const;
+
+export const COMMUNICATION_VERBS_SPANISH = [
+  "Redacté",
+  "Colaboré",
+  "Convencí",
+  "Edité",
+  "Ilustré",
+  "Negocié",
+  "Persuadí",
+  "Presenté",
+  "Publiqué",
+  "Difundí",
+] as const;
+
+export const CREATIVE_VERBS_SPANISH = [
+  "Conceptualicé",
+  "Creé",
+  "Personalicé",
+  "Integré",
+  "Inventé",
+  "Modifiqué",
+  "Originé",
+  "Reformateé",
+  "Revitalicé",
+  "Ideé",
+] as const;
+
+export const HARVARD_ACTION_VERBS_SPANISH = [
+  ...LEADERSHIP_VERBS_SPANISH,
+  ...TECHNICAL_VERBS_SPANISH,
+  ...COMMUNICATION_VERBS_SPANISH,
+  ...CREATIVE_VERBS_SPANISH,
+];
